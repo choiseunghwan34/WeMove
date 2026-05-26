a64bed24a8e6eec2df09809b5acc1b0ba2cf1f95
kimnayoung7777 <k9075k@gmail.com>
2026-05-26 14:54:12 +0900

    .

--- frontend/src/pages/MeetingEditPage.jsx
+++ frontend/src/pages/MeetingEditPage.jsx
@@ -1,6 +1,15 @@
 ﻿import MeetingFormPage from "./MeetingFormPage.jsx";
+import {useNavigate, useParams} from "react-router-dom";
+import {useState} from "react";
 
 export default function MeetingEditPage() {
+
+  const {meetingId} = useParams();
+  const navigate = useNavigate();
+  const [initialData, setInitialData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
   const handleUpdate = (formData) => { /* API 로직 */ };
   return <MeetingFormPage title="모임 수정하기" onSubmit={handleUpdate} />;
 }
